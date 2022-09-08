e8ad379dfa939246bd233cda6d314f566360e34d
Henry Roberts <whroberts5@gmail.com>
2022-09-08 17:29:35 -0500

    more changes

--- Assets/_Game/MovementTesting/MovementController.cs
+++ Assets/_Game/MovementTesting/MovementController.cs
@@ -1,58 +1,132 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MovementController : MonoBehaviour
 {
+    [SerializeField] private Transform _groundCheck = null;
     [SerializeField] private Vector3 _centerOfMass = Vector3.zero;
+
+    [Header("Standard Movement")]
     [SerializeField] private float _acceleration = 0f;
     [SerializeField] private float _maxSpeed = 0f;
+    [SerializeField] private float _standardTurnSpeed = 0.5f;
+
+    [Header("Drifting")]
+    [SerializeField] private float _driftTurnSpeed = 0.1f;
+
+    [Header("Boosting")] 
+    [SerializeField] private float _boostAcceleration = 0f;
+    [SerializeField] private float _boostMaxSpeed = 0f;
+    [SerializeField] private float _boostDuration = 2f;
+    [SerializeField] private float _boostCooldown = 2f;
+    [SerializeField] private float _boostRemaining = 2f;
+    [SerializeField] private bool _boostOnCooldown = false;
 
     private float _turnSmoothVel;
+    private bool _isMoving = false;
     private Vector3 _direction;
+    private float _currentTurnSpeed;
 
     private Rigidbody _rb;
-    private Inputs _inputs;
+    private MovementControls _movementControls;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
         _rb.centerOfMass = _centerOfMass;
 
-        _inputs = GetComponent<Inputs>();
+        _movementControls = GetComponent<MovementControls>();
     }
 
     private void FixedUpdate()
     {
-        Steer();
+        if (!IsGrounded() || _isMoving) Steer();
 
-        if (_inputs.Speed > 0f) Drive();
+        Drive();
+
     }
 
     private void Steer()
     {
-        _direction = new Vector3(_inputs.DirectionVector.x, 0f, _inputs.DirectionVector.y);
+        _direction = new Vector3(_movementControls.DirectionVector.x, 0f, _movementControls.DirectionVector.y);
 
         if (_direction.magnitude >= 0.1f)
         {
             var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
-            var angle = Mathf.SmoothDampAngle(_rb.rotation.eulerAngles.y, targetAngle, ref _turnSmoothVel, 0.1f);
+            var angle = Mathf.SmoothDampAngle(_rb.rotation.eulerAngles.y, targetAngle, ref _turnSmoothVel, _currentTurnSpeed);
             _rb.MoveRotation(Quaternion.Euler(0f, angle, 0f));
         }
     }
 
     private void Drive()
     {
-        _rb.AddForce(_direction * (_acceleration * _inputs.Speed), ForceMode.Acceleration);
-        _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, _maxSpeed);
+
+        if (_movementControls._movementInput.Player.Drive.IsPressed())
+        {
+            _rb.AddForce(transform.forward * (_acceleration * _movementControls.Speed), ForceMode.Acceleration);
+            _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, _maxSpeed);
+
+            if (_boostRemaining != _boostDuration) StartCoroutine(BoostCooldown());
+
+        }
+
+        if (_movementControls.Boost && !_boostOnCooldown)
+        {
+            _rb.AddForce(transform.forward * _boostAcceleration, ForceMode.Acceleration);
+            _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, _boostMaxSpeed);
+
+            BoostTimer();
+        }
+
+        if (_rb.velocity == Vector3.zero)
+        {
+            _isMoving = false;
+        }
+        else _isMoving = true;
+
+        Drift();
+
     }
 
     private void Drift()
     {
-        if (_inputs.Drift)
+        if (_movementControls.Drift)
         {
-
+            _currentTurnSpeed = _driftTurnSpeed;
         }
+        else
+        {
+            _currentTurnSpeed = _standardTurnSpeed;
+        }
+    }
+
+    private IEnumerator BoostCooldown()
+    {
+        _boostOnCooldown = true;
+        _boostRemaining = _boostDuration;
+        yield return new WaitForSeconds(_boostCooldown);
+        _boostOnCooldown = false;
+    }
+
+
+    private void BoostTimer()
+    {
+        if (_boostRemaining > 0)
+        {
+            _boostRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            _boostRemaining = 0;
+            StartCoroutine(BoostCooldown());
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.CheckSphere(_groundCheck.position, 0.1f);
     }
 
     private void OnDrawGizmos()
---
+++ Assets/_Game/MovementTesting/MovementControls.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementControls : MonoBehaviour
+{
+    public MovementInput _movementInput;
+
+    private Vector2 _directionVector;
+    public Vector2 DirectionVector => _directionVector;
+
+    private float _speed;
+    public float Speed => _speed;
+
+    private bool _drift;
+    public bool Drift => _drift;
+
+    private bool _boost;
+    public bool Boost => _boost;
+
+    private void Awake()
+    {
+        _movementInput = new MovementInput();
+    }
+
+    private void OnEnable()
+    {
+        _movementInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _movementInput.Disable();
+    }
+
+    private void Update()
+    {
+        _directionVector = _movementInput.Player.Direction.ReadValue<Vector2>();
+        _speed = _movementInput.Player.Drive.ReadValue<float>();
+        _drift = _movementInput.Player.Drift.IsPressed();
+        _boost = _movementInput.Player.Boost.IsPressed();
+    }
+}
