86d5d88b85d26966bb2b80cc6516c1b855b718dd
Sai Mangipudi <thegeekskm101@gmail.com>
2022-09-12 23:11:40 -0500

    Adjusted Fracture to be on Child instead of Parent
    Did Sai's work for him

--- Assets/_Game/Levels/Testing/Level Mechanics/SwitchToFracture.cs
+++ Assets/_Game/Levels/Testing/Level Mechanics/SwitchToFracture.cs
@@ -8,24 +8,20 @@ public class SwitchToFracture : MonoBehaviour
     [SerializeField] private GameObject _brokenVersion;
     [SerializeField] private GameObject _unbrokenVersion;
     [SerializeField] private FixedJoint _joint;
+    [SerializeField, ReadOnly] private bool _isFractured = false;
     
     private void OnValidate()
     {
-        if (_unbrokenVersion == null)
-        {
-            _unbrokenVersion = transform.GetChild(0).gameObject;
-        }
-        if (_brokenVersion == null)
-        {
-            _brokenVersion = transform.GetChild(1).gameObject;
-        }
+        
+       
         if (_joint == null) _joint = GetComponent<FixedJoint>();
     }
 
     private void Start() 
     {
         _unbrokenVersion.SetActive(true);
-        _brokenVersion.SetActive(false);    
+        _brokenVersion.SetActive(false);   
+        _isFractured = false; 
     }
 
     private void Update()
@@ -35,9 +31,13 @@ public class SwitchToFracture : MonoBehaviour
 
     private void SetFractured()
     {
-        _unbrokenVersion.SetActive(false);
-        _brokenVersion.SetActive(true);
-        Destroy(gameObject);
+        if (!_isFractured)
+        {
+            _unbrokenVersion.SetActive(false);
+            _brokenVersion.SetActive(true);
+            _isFractured = true; 
+            Destroy(gameObject);
+        }
     }
 
     /*
