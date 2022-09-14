62491df722588165a7d815f6a899300f993e5817
Henry Roberts <whroberts5@gmail.com>
2022-09-14 17:51:17 -0500

    side flip and physics turn first pass

--- Assets/_Game/Levels/Testing/Movement/MovementController.cs
+++ Assets/_Game/Levels/Testing/Movement/MovementController.cs
@@ -1,8 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
-using UnityEditor.Rendering;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Quaternion = UnityEngine.Quaternion;
@@ -16,7 +15,11 @@ public class MovementController : MonoBehaviour
     [SerializeField] private Transform _groundCheck;
     [SerializeField] private float _groundCheckRadius = 0.1f;
     [SerializeField] private LayerMask _groundLayer;
-    
+
+    [Header("Wall Bounce")]
+    [SerializeField] private LayerMask _wallLayer;
+    [SerializeField] private float _upwardBounce;
+
     [Header("Movement")]
     [SerializeField] private float _acceleration = 25;
     [SerializeField] private float _maxSpeed = 25;
@@ -25,6 +28,11 @@ public class MovementController : MonoBehaviour
     [SerializeField] private bool _turnWhenStopped = true;
     [SerializeField] private float _stoppedTurnSpeed = 1.0f;
     [SerializeField] private float _standardTurnSpeed = 0.75f;
+
+    [Header("Side Flip")]
+    [SerializeField] private float _flipLaunch;
+    [SerializeField] private float _flipLaunchTorque;
+    [SerializeField] private float _newDrag;
 
     [Header("Drifting")]
     [SerializeField] private float _driftTurnSpeed = 0.25f;
@@ -48,6 +56,7 @@ public class MovementController : MonoBehaviour
     [SerializeField, ReadOnly] private bool _isGrounded;
     [SerializeField, ReadOnly] private bool _isDrifting;
     [SerializeField, ReadOnly] private bool _isBoosting;
+    [SerializeField, ReadOnly] private bool _isFlipping;
     [SerializeField, ReadOnly] private Vector3 _direction;
     [SerializeField, ReadOnly] private Vector3 _previousVel;
 
@@ -73,7 +82,7 @@ public class MovementController : MonoBehaviour
     private void FixedUpdate()
     {
         _isGrounded = IsGrounded();
-        if (!_isGrounded || _turnWhenStopped) Steer();
+        if ((_isGrounded || _turnWhenStopped) && !_isFlipping) Steer();
 
         if (_isGrounded) Movement();
 
@@ -102,18 +111,30 @@ public class MovementController : MonoBehaviour
         if (_direction.magnitude >= 0.1f)
         {
             
+            
             var currentAngle = _rb.rotation.eulerAngles.y;
-            //var targetAngle = currentAngle + (_direction.x - _direction.z) * Mathf.Rad2Deg;
             var targetAngle = currentAngle + Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
             var angle = Mathf.SmoothDampAngle(currentAngle, targetAngle, ref _turnSmoothVel, _currentTurnSpeed);
-            _rb.MoveRotation(Quaternion.Euler(0f, angle, 0f));
+            //_rb.MoveRotation(Quaternion.Euler(0f, angle, 0f));
+
+            /*
+            var torque = Vector3.Cross(_rb.transform.forward, new Vector3(angle,0f,0f)) / Time.fixedDeltaTime;
+            Debug.Log("Direction: " + _direction);
+            Debug.Log("Cross: " + torque);
+            _rb.AddRelativeTorque(torque,ForceMode.VelocityChange);
+            */
+
+            var torque = _rb.transform.up * 100 * angle * Time.fixedDeltaTime;
+            Debug.Log("Torque: " + torque);
+            _rb.maxAngularVelocity = _currentTurnSpeed;
+            _rb.AddRelativeTorque(torque,ForceMode.VelocityChange);
+
+
         }
     }
 
     private void Movement()
     {
-        Vector3 forceVector = transform.forward * (_currentAcceleration * _movementControls.Speed);
-
         if (_movementControls.Boost && !_boostOnCooldown) Boost();
         else if (_movementControls.Speed != 0) Drive();
 
@@ -122,9 +143,10 @@ public class MovementController : MonoBehaviour
         _rb.AddForce(transform.forward * (_currentAcceleration * _movementControls.Speed), ForceMode.Acceleration);
         _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, _currentMaxSpeed);
 
-        _isMoving = _rb.velocity.magnitude > 0.05f;
+        _isMoving = _rb.velocity.magnitude > 0.1f;
 
         Drift();
+        SideFlip();
 
     }
 
@@ -136,6 +158,9 @@ public class MovementController : MonoBehaviour
 
     private void Boost()
     {
+
+        if (_movementControls.Speed <= 0) return;
+
         _currentAcceleration = _boostAcceleration;
         _currentMaxSpeed = _boostMaxSpeed;
 
@@ -176,9 +201,39 @@ public class MovementController : MonoBehaviour
         }
     }
 
+    private void SideFlip()
+    {
+        if (_movementControls.SideFlip != 0) StartCoroutine(FlipForces(_movementControls.SideFlip));
+    }
+
+    private IEnumerator FlipForces(float direction)
+    {
+        _isFlipping = true;
+        _rb.velocity = Vector3.zero;
+        var tempDrag = _rb.angularDrag;
+
+        _rb.AddRelativeForce(new Vector3(direction*_flipLaunch, _flipLaunch, 0f), ForceMode.VelocityChange);
+        yield return new WaitForSeconds(0.05f);
+
+        _rb.angularDrag = _newDrag;
+        _rb.AddRelativeTorque(new Vector3(0f, 0f, -direction*_flipLaunchTorque), ForceMode.VelocityChange);
+
+        yield return new WaitUntil(IsGrounded);
+        _rb.angularDrag = tempDrag;
+        _isFlipping = false;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (!collision.collider.GetComponent<Wall>()) return;
+        ExaggeratedWallBounce(collision);
+    }
+
+
+
+    //Bounce with direct momentum transfer
+    private void StandardWallBounce(Collision collision)
+    {
+        if (collision.gameObject.layer != _wallLayer) return;
         var vel = _previousVel;
         vel.x = -vel.x;
         vel.y = 1;
@@ -186,6 +241,17 @@ public class MovementController : MonoBehaviour
         var rot = Quaternion.Lerp(_rb.rotation, Quaternion.Euler(Vector3.forward), 0.5f);
         _rb.MoveRotation(rot);
         _rb.angularVelocity = Vector3.zero;
+    }
+
+    // Wall Bounce with exaggerated momentum transfer
+    private void ExaggeratedWallBounce(Collision collision)
+    {
+        if (collision.gameObject.layer != _wallLayer) return;
+
+        var initalContactPoint = collision.GetContact(0).point;
+
+        _rb.AddForceAtPosition(new Vector3(0,_upwardBounce,0), initalContactPoint, ForceMode.Impulse);
+        _rb.AddForceAtPosition(collision.impulse, initalContactPoint, ForceMode.Impulse);
     }
 
     private void OnDrawGizmos()
