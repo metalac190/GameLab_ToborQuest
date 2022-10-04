87ebb8a9e53ab727b0763a79c7920c77408c5207
Brandon Coffey <bmc939@gmail.com>
2022-10-04 15:18:48 -0500

    Dialogue and fixes

---
+++ Assets/_Game/Scripts/Mechanics/Dialogue/Dialogue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Dialogue : ScriptableObject
+{
+	[SerializeField] private string _speaker;
+	[SerializeField, TextArea] private string _text;
+	
+	public string Speaker => _speaker;
+	public string Text => _text;
+	
+	[Button]
+	public void RunDialogue()
+	{
+		if (DialogueSystem.Instance) DialogueSystem.Instance.RunDialogue(this);
+		else Debug.LogWarning("No Dialogue Sytstem in Place");
+	}
+}
