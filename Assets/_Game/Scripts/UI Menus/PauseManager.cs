2e581b87c67044e3e3476286ee06560a80e518ad
Brandon Coffey <bmc180001@utdallas.edu>
2022-10-04 01:31:12 -0500

    Build Pre-Alpha!

--- Assets/_Game/Scripts/UI Menus/PauseManager.cs
+++ Assets/_Game/Scripts/UI Menus/PauseManager.cs
@@ -37,6 +37,11 @@ public class PauseManager : MonoBehaviour
         CGSC.LoadMainMenu();        
     }
 
+    public void Restart()
+    {
+        CGSC.RestartLevel();
+    }
+
     public void ReturnToLevels()
     {
         CGSC.UnpauseGame();
