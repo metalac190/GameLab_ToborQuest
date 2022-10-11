547cadacddd8b1a315d188d56692091a90b55fb3
Jonathan Gonzalez <jonathansgonz03@gmail.com>
2022-10-10 22:08:57 -0500

    Saving in all scenes working

---
+++ Assets/_Game/Scripts/UI Menus/MenuLevelInfoPanel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuLevelInfoPanel : MonoBehaviour
+{
+    [SerializeField]
+    private string levelSaveName;
+    private TextMeshProUGUI bestTimeText;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
