b1b0654dafe4d29e66c2a5995824676fa60a2c74
Sai Mangipudi <thegeekskm101@gmail.com>
2022-10-10 21:21:54 -0500

    Win/Respawn Fields use InvisibleTrigger, Dialogue Trigger Created

--- Assets/_Game/Scripts/Mechanics/Dialogue/Dialogue.cs
+++ Assets/_Game/Scripts/Mechanics/Dialogue/Dialogue.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu]
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "DialogueSystem/Dialogue")]
 public class Dialogue : ScriptableObject
 {
 	[SerializeField] private string _speaker;
