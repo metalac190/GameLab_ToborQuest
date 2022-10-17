998efd8264d536a6ef2b1ced5470894d78d6d0f7
Jonathan Gonzalez <jonathansgonz03@gmail.com>
2022-10-17 16:30:50 -0500

    temporary commit

--- Assets/_Game/Scripts/UI Menus/LevelInfoManager.cs
+++ Assets/_Game/Scripts/UI Menus/LevelInfoManager.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 public class LevelInfoManager : MonoBehaviour
 {
     public Image levelPreviewImage;
     public TextMeshProUGUI levelDescriptionText;
-
     public Button onStart;
     private LevelInfoObject levelInfoObj;
+    [SerializeField] private TextMeshProUGUI bestTimeText;
 
-    private void SetLevelInfoObject(LevelInfoObject value)
+    private void Awake()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void SetLevelInfoObject(LevelInfoObject value)
     {
         levelInfoObj = value;
+        this.gameObject.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(onStart.gameObject);
     }
 
     private void OnEnable()
@@ -25,7 +32,13 @@ public class LevelInfoManager : MonoBehaviour
 
     private void SetInfo(LevelInfoObject value)
     {
-        
+        levelPreviewImage.sprite = levelInfoObj.levelImage;
+        levelDescriptionText.text = levelInfoObj.levelInfo;
+        bestTimeText.text = levelInfoObj.GetTimeFormatted();
+        onStart.onClick.AddListener(() =>
+        {
+            CGSC.LoadScene(value.GetLevelSceneName());
+        });
     }
 
 }
--- Assets/_Game/Scripts/UI Menus/LevelInfoObject.cs
+++ Assets/_Game/Scripts/UI Menus/LevelInfoObject.cs
@@ -1,19 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 [CreateAssetMenu()]
 public class LevelInfoObject : ScriptableObject
 {
     [SerializeField] private SerializedScene levelScene;
-    [SerializeField] private string levelInfo;
-    [SerializeField] private Image levelImage;
+    public string levelInfo;
+    public Sprite levelImage;
     public float BestTime { get; set; }
     public bool ghostDataAvailable { get; set; }
+    private string levelSaveTimeName;
+    public bool removeWhiteSpace = false;
 
-    public SerializedScene GetLevelScene()
+    public string GetLevelSceneName()
     {
-        return levelScene;
+        return levelScene.Name;
+    }
+
+    public string GetTimeFormatted()
+    {
+        Debug.Log(removeWhiteSpace);
+        if(removeWhiteSpace)
+            levelSaveTimeName = levelScene.Name.Remove(5,1) + "BestTime";
+        else
+            levelSaveTimeName = levelScene.Name + "BestTime";
+
+        BestTime = PlayerPrefs.GetFloat(levelSaveTimeName);
+        Debug.Log(levelSaveTimeName + " " + BestTime);
+        TimeSpan time = TimeSpan.FromSeconds(BestTime);
+        if (BestTime > 3600)
+            return time.ToString(@"hh\:mm\:ss\:fff");
+        else
+            return time.ToString(@"mm\:ss\:fff");
     }
 }
