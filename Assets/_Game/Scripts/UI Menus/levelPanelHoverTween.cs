b9a28889420b25f0d4ca4cf89580bc5056d55d73
Jonathan Gonzalez <jonathansgonz03@gmail.com>
2022-10-17 20:04:25 -0500

    fixed some UI Issues and some small improvements

--- Assets/_Game/Scripts/UI Menus/levelPanelHoverTween.cs
+++ Assets/_Game/Scripts/UI Menus/levelPanelHoverTween.cs
@@ -2,28 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class levelPanelHoverTween : MonoBehaviour, ISelectHandler, IDeselectHandler
+public class levelPanelHoverTween : MonoBehaviour, ISelectHandler, IDeselectHandler, ISubmitHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public float widthScale;
     public float heightScale;
     public float duration;
-
+    public LeanTweenType expandTweenType = LeanTweenType.easeOutCubic;
+    public LeanTweenType resetTweenType = LeanTweenType.easeOutSine;
     private RectTransform _transform;
+    private Button _button;
 
     private void Awake()
     {
         _transform = this.GetComponent<RectTransform>();
+        _button = this.GetComponent<Button>();
     }
 
     public void ExpandTween()
     {
-        _transform.LeanScale(new Vector3(widthScale,heightScale, 1), duration);
+        _transform.LeanScale(new Vector3(widthScale,heightScale, 1), duration).setEase(expandTweenType);
     }
 
-    public void ResetScale()
+    public void ResetTween()
     {
-        _transform.LeanScale(new Vector3(1, 1, 1), duration);
+        _transform.LeanScale(new Vector3(1, 1, 1), duration).setEase(resetTweenType);
     }
 
     public void OnSelect(BaseEventData eventData)
@@ -33,6 +37,22 @@ public class levelPanelHoverTween : MonoBehaviour, ISelectHandler, IDeselectHand
 
     public void OnDeselect(BaseEventData eventData)
     {
-        ResetScale();
+        ResetTween();
+    }
+
+    public void OnSubmit(BaseEventData eventData)
+    {
+        _button.onClick.Invoke();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ExpandTween();
+        EventSystem.current.SetSelectedGameObject(this.gameObject);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ResetTween();
     }
 }
