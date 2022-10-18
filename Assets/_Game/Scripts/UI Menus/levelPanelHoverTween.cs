0494cd1dc6ff2d2321d085f541573ce74eb3f31a
Jonathan Gonzalez <jonathansgonz03@gmail.com>
2022-10-17 19:52:24 -0500

    LevelObjects Tweens

--- Assets/_Game/Scripts/UI Menus/levelPanelHoverTween.cs
+++ Assets/_Game/Scripts/UI Menus/levelPanelHoverTween.cs
@@ -1,21 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class levelPanelHoverTween : MonoBehaviour
+public class levelPanelHoverTween : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
     public float widthScale;
     public float heightScale;
+    public float duration;
 
-    // Start is called before the first frame update
-    void Start()
+    private RectTransform _transform;
+
+    private void Awake()
     {
-        
+        _transform = this.GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExpandTween()
     {
-        
+        _transform.LeanScale(new Vector3(widthScale,heightScale, 1), duration);
+    }
+
+    public void ResetScale()
+    {
+        _transform.LeanScale(new Vector3(1, 1, 1), duration);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        ExpandTween();
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        ResetScale();
     }
 }
