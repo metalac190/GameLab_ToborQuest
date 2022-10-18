473ad1d508912c582b6f0f1fda6db47d60b8dbdd
Sai Mangipudi <thegeekskm101@gmail.com>
2022-10-17 23:04:15 -0500

    Finished Respawn Field and Checkpoints

--- Assets/_Game/Scripts/Mechanics/Level Mechanics Scripts/RespawnField.cs
+++ Assets/_Game/Scripts/Mechanics/Level Mechanics Scripts/RespawnField.cs
@@ -2,24 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RespawnField : MonoBehaviour
+public class RespawnField : InvisibleTrigger
 {
-    [SerializeField] Transform _spawnTransform;
-    [SerializeField] BoxCollider _collider;
+    #region Do not read...trust me...
+    // [SerializeField] Transform _spawnTransform;
+    // [SerializeField] BoxCollider _collider;
 
-    void OnValidate()
-    {
-        if (_collider == null) { _collider = GetComponent<BoxCollider>(); }
-    }
-    Transform UsedTransform =>(_spawnTransform != null) ? _spawnTransform : transform;
-    void OnTriggerEnter(Collider other)
-    {
-        SetCheckpoint(other.gameObject);
-    }
+    // void OnValidate()
+    // {
+    //     if (_collider == null) { _collider = GetComponent<BoxCollider>(); }
+    // }
+    // Transform UsedTransform =>(_spawnTransform != null) ? _spawnTransform : transform;
+    // void OnTriggerEnter(Collider other)
+    // {
+    //     SetCheckpoint(other.gameObject);
+    // }
 
-    public void SetCheckpoint(GameObject otherObject)
+    // public void SetCheckpoint(GameObject otherObject)
+    // {
+    //     var _checkpointTracker = otherObject.gameObject.GetComponent<CheckpointTracker>();
+    //     if (_checkpointTracker != null) { _checkpointTracker._recentCheckpoint = UsedTransform; }
+    // }
+    #endregion
+
+    [SerializeField] bool _respawnAtCheckpoint = false;
+
+    protected override void OnPlayerTrigger(Collider other)
     {
-        var _checkpointTracker = otherObject.gameObject.GetComponent<CheckpointTracker>();
-        if (_checkpointTracker != null) { _checkpointTracker._recentCheckpoint = UsedTransform; }
+        var _checkpointTracker = other.gameObject.GetComponent<CheckpointTracker>();
+
+       if (_respawnAtCheckpoint && _checkpointTracker != null)
+       {
+             _checkpointTracker.Respawn();
+             Debug.Log($"[RESPAWN FIELD] Respawned!");
+       }
+       else if (!_respawnAtCheckpoint && _checkpointTracker != null)
+       {
+             _checkpointTracker.SetCheckpoint(transform);
+             Debug.Log($"[RESPAWN FIELD] Set the checkpoint at {transform.position}");
+       }
+        base.OnPlayerTrigger(other);
     }
 }
