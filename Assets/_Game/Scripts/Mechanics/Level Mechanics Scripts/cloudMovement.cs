811b9fa7bdb178ad48382a5582b2665a88dde3c6
Tate <taterileyberry@gmail.com>
2022-12-01 21:36:58 -0600

    Don't merge Unless we get the OK from prof

--- Assets/_Game/Scripts/Mechanics/Level Mechanics Scripts/cloudMovement.cs
+++ Assets/_Game/Scripts/Mechanics/Level Mechanics Scripts/cloudMovement.cs
@@ -4,61 +4,52 @@ using UnityEngine;
 
 public class cloudMovement : MonoBehaviour
 {
-    float _moveSpeed;
-    float _startSpeed;
     Vector3 _startPos;
     Vector3 _endPos;
+    public AnimationCurve MoveCruve;
+    float _animationTimePos;
+    float _speed;
+    bool _movingLeft;
 
     private void Start()
     {
         _startPos = transform.position;
         _endPos = new Vector3 (transform.position.x-200, transform.position.y, transform.position.z);
-        _moveSpeed = Random.Range(8, 12);
-        _startSpeed = _moveSpeed;
+        _movingLeft = true;
+        _speed = Random.Range(0.0150f, 0.0275f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //cloudMove();
+        cloudMove();
     }
-    /*
     void cloudMove()
     {
-        
+        if (_movingLeft == false)
+        {
+            _animationTimePos += Time.deltaTime * _speed;
+            transform.position = Vector3.Lerp(_endPos, _startPos, MoveCruve.Evaluate(_animationTimePos));
+        }
+        if (_movingLeft == true)
+        {
+            _animationTimePos += Time.deltaTime * _speed;
+            transform.position = Vector3.Lerp(_startPos, _endPos, MoveCruve.Evaluate(_animationTimePos));
+        }
         if (transform.position.x <= _endPos.x)
         {
-            StartCoroutine(decelearte(1));
-            transform.position = new Vector3(_endPos.x + .1f, transform.position.y, transform.position.z);
+            _movingLeft = false;
+            transform.position = new Vector3(_endPos.x + 1f, _endPos.y, _endPos.z);
+            _speed = Random.Range(0.01f, 0.03f);
+            _animationTimePos = 0;
         }
         if (transform.position.x >= _startPos.x)
         {
-            StartCoroutine(decelearte(-1));
-            transform.position = new Vector3(_startPos.x - .1f, transform.position.y, transform.position.z);
+            _movingLeft = true;
+            transform.position = new Vector3(_startPos.x - 1f, _startPos.y, _startPos.z);
+            _speed = Random.Range(0.01f, 0.03f);
+            _animationTimePos = 0;
         }
-        transform.position = transform.position + new Vector3(_moveSpeed * Time.deltaTime, 0, 0);
     }
 
-    
-    IEnumerator decelearte(float directionNum)
-    {
-        float setupSpeed = _moveSpeed;
-        for (float speed = setupSpeed; speed >= 0; speed -= 0.1f)
-        {
-            _moveSpeed = speed;
-            yield return null;
-        }
-        StartCoroutine(accelerate(directionNum));
-    }
-
-    IEnumerator accelerate(float directionNum)
-    {
-        _startSpeed = directionNum;
-        for (float speed = 0; speed <= _startSpeed; speed += 0.1f)
-        {
-            _moveSpeed = speed;
-            yield return null;
-        }
-    }
-    */
 }
