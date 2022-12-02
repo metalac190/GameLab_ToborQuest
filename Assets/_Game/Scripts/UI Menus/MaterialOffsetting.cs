ab7cb71e2a3cf661896313afc56c74aea42a0950
Jonathan Gonzalez <jonathansgonz03@gmail.com>
2022-12-02 00:08:47 -0600

    New scene transition for sliders

--- Assets/_Game/Scripts/UI Menus/MaterialOffsetting.cs
+++ Assets/_Game/Scripts/UI Menus/MaterialOffsetting.cs
@@ -6,24 +6,33 @@ using UnityEngine.UI;
 public class MaterialOffsetting : MonoBehaviour
 {
     public float speed = 0.1f;
-    public bool moveToRight = true;
+    public bool moveToLeft = true;
     private Image posterImage;
     private Material imageMaterial;
-
+    private bool toggle = false;
     // Start is called before the first frame update
     void Awake()
     {
-        if (!moveToRight)
+        if (!moveToLeft)
             speed *= -1;
         posterImage = GetComponent<Image>();
         imageMaterial = posterImage.material;
+        //Debug.Log("Random number is " + Random.Range(-100, 100));
         imageMaterial.mainTextureOffset = new Vector2(Random.Range(-100, 100), 0);
         //posterImage.material.mainTextureOffset = new Vector2(Random.Range(0, 100), 0);
+    }
+
+    private void OnDisable()
+    {
+        toggle = !toggle;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        imageMaterial.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0);
+        if(toggle)
+            imageMaterial.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0);
+        else
+            imageMaterial.mainTextureOffset -= new Vector2(speed * Time.deltaTime, 0);
     }
 }
